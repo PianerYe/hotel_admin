07ee13fe8ae73c3cce86585612950976096cee57
yepianer <aimeinvshen@126.com>
2023-03-30 16:51:30 +0800

    RestClient操作索引 创建索引库

--- hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,13 +1,18 @@
 package cn.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static cn.itcast.hotel.constants.HotelConstants.MAPPING_TEMPLATE;
 
 public class HotelIndexTest {
     private RestHighLevelClient client;
@@ -15,6 +20,16 @@ public class HotelIndexTest {
     @Test
     void testInit(){
         System.out.println(client);
+    }
+
+    @Test
+    void testCreateHotelIndex() throws IOException {
+        //1.创建Request对象
+        CreateIndexRequest request = new CreateIndexRequest("hotel");
+        //2.请求参数，MAPPING_TEMPLATE是静态常量字符串，内容是创建索引库的DSL语句
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+        //3.发起请求
+        client.indices().create(request, RequestOptions.DEFAULT);
     }
 
     @BeforeEach
