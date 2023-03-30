abbaea363610248abb67241c823a53308b0486ab
yepianer <aimeinvshen@126.com>
2023-03-30 17:01:03 +0800

    RestClient操作索引 删除和判断索引库

--- hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,10 +1,12 @@
 package cn.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,6 +32,24 @@ public class HotelIndexTest {
         request.source(MAPPING_TEMPLATE, XContentType.JSON);
         //3.发起请求
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        //1.创建Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("hotel");
+        //2.发起请求
+        client.indices().delete(request,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        //1.创建Request对象
+        GetIndexRequest request = new GetIndexRequest("hotel");
+        //2.发起请求
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        //3.输出
+        System.out.println(exists ? "索引库已经存在" : "索引库不存在！");
     }
 
     @BeforeEach
