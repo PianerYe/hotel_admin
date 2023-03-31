dc313c4f22ea2b8d5b1d0dbe3e05975dbfa152ce
yepianer <aimeinvshen@126.com>
2023-03-31 09:34:37 +0800

    RestClient操作文档 查询，更新文档

--- hotel-demo/src/test/java/cn/itcast/hotel/HotelDocumentTest.java
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelDocumentTest.java
@@ -5,7 +5,11 @@ import cn.itcast.hotel.pojo.HotelDoc;
 import cn.itcast.hotel.service.IHotelService;
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -36,6 +40,33 @@ public class HotelDocumentTest {
         request.source(JSON.toJSONString(hotelDoc), XContentType.JSON);
         //3。发送请求
         client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGetDocumentById() throws IOException {
+        //1.准备Request对象
+        GetRequest request = new GetRequest("hotel","61083");
+        //2。发送请求
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        //3. 解析结果
+        String json = response.getSourceAsString();
+
+        HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
+
+        System.out.println(hotelDoc);
+    }
+
+    @Test
+    void testUpdateDocument() throws IOException {
+        //1.准备Request对象
+        UpdateRequest request = new UpdateRequest("hotel","61083");
+        //2. 准备一个参数，每两个参数为一对key value
+        request.doc(
+                "price",952,
+                "starName","四钻"
+        );
+        //3。更新文档
+        UpdateResponse response = client.update(request, RequestOptions.DEFAULT);
     }
 
     @BeforeEach
