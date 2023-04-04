bb4fb5155fbeff2dbf821219d0aaa1f1c28d008f
yepianer <aimeinvshen@126.com>
2023-04-04 11:45:38 +0800

    RestClient查询文档 高亮显示

--- hotel-demo/src/test/java/cn/itcast/hotel/HotelSearchTest.java
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelSearchTest.java
@@ -3,27 +3,28 @@ package cn.itcast.hotel;
 import cn.itcast.hotel.pojo.HotelDoc;
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
-import org.apache.lucene.search.TotalHits;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 public class HotelSearchTest {
@@ -116,6 +117,21 @@ public class HotelSearchTest {
         handleResponse(response);
     }
 
+    @Test
+    void testHignlight() throws IOException {
+        //1.准备Request对象
+        SearchRequest request = new SearchRequest("hotel");
+        //2。准备DSL
+        //2.1 query
+        request.source().query(QueryBuilders.matchQuery("all","如家"));
+        //2.2 highlight
+        request.source().highlighter(new HighlightBuilder().field("name").requireFieldMatch(false));
+        //3. 发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4. 解析响应
+        handleResponse(response);
+    }
+
 
 
     private void handleResponse(SearchResponse response) {
@@ -132,8 +148,20 @@ public class HotelSearchTest {
             String json = hit.getSourceAsString();
             // 反序列化
             HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
-            System.out.println("hotelDoc =" + hotelDoc);
-        }
+            //获取高亮字段结果
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if (CollectionUtils.isEmpty(highlightFields)){
+                // 根据字段名获取高亮结果
+                HighlightField highlightField = highlightFields.get("name");
+                if (highlightField != null){
+                    // 取出高亮结果数组中的第一个，就是酒店名称
+                    String name = highlightField.getFragments()[0].string();
+                    hotelDoc.setName(name);
+                }
+                System.out.println("hotelDoc =" + hotelDoc);
+                }
+            }
+
     }
 
     @BeforeEach
